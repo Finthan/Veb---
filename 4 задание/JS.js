f573e061be0dd5fc93910f3abdccd180ab0f95a1
Finthan <41282788+Finthan@users.noreply.github.com>
2020-05-29 17:46:15 +1000

    Add files via upload

--- 4 задание/JS.js
+++ 4 задание/JS.js
@@ -1,4 +1,4 @@
-function java1(obj)
+function SquareJava1(obj)
 {
     var a = obj.num1.value;
     var h = obj.num2.value;
@@ -12,6 +12,90 @@ function java1(obj)
     else
     {
         obj.res.value = 'Error';
+    }
+}
+
+
+
+function SquareJava2(obj) {
+    var b = obj.num1.value;
+    var h = obj.num2.value;
+    var s = b * h;
+    if (s > 0) {
+        obj.res.value = s;
+    }
+    else {
+        obj.res.value = 'Error';
+    }
+}
+
+function SquareJava3(obj) {
+    var a = obj.num1.value;
+    var b = obj.num2.value;
+    var sin = obj.num3.value;
+    if (sin > 90 && sin < -90) {
+        obj.res.value = 'Error';
+    }
+    else {
+        sin = (sin * Math.PI) / 180;
+        var s = (a * b * Math.sin(sin)).toFixed(3);
+        if (s > 0) {
+            obj.res.value = s;
+        }
+        else {
+            obj.res.value = 'Error';
+        }
+    }
+
+}
+
+function SquareJava4(obj) {
+    var a = obj.num1.value;
+    var b = obj.num2.value;
+    var sin = obj.num3.value;
+    if (sin > 90) {
+        obj.res.value = 'Error';
+    }
+    else {
+        sin = (sin * Math.PI) / 180;
+        var s = ((a * b * Math.sin(sin)) / 2).toFixed(3);
+        if (s > 0) {
+            obj.res.value = s;
+        }
+        else {
+            obj.res.value = 'Error';
+        }
+    }
+}
+
+function SquareJava5(obj) {
+    var a = obj.num1.value;
+    var r = obj.num2.value;
+    var s = 2 * a * r;
+    if (s > 0) {
+        obj.res.value = s;
+    }
+    else {
+        obj.res.value = 'Error';
+    }
+}
+
+function SquareJava6(obj) {
+
+    var r = obj.num2.value;
+    var sin = obj.num1.value;
+    if (sin > 90) {
+        obj.res.value = 'Error';
+    }
+    else {
+        sin = (sin * Math.PI) / 180;
+        var s = ((4 * r * r) / Math.sin(sin)).toFixed(3);
+        if (s > 0) {
+            obj.res.value = s;
+        }
+        else {
+            obj.res.value = 'Error';
+        }
     }
 }
 
@@ -50,26 +134,123 @@ function java2(obj)
 
 function array()
 {
-    var data = document.getElementsById("value");
+    var data = document.getElementsByName("value");
     var sum;
-    var result;
+    var result = [];
+    var input = [];
 
-    for (var i = 0; i < data.length; i++)
+    for( var i = 0; i < data.length; i++)
+    {
+        var row = parseInt(data[i].getAttribute("row"));
+
+        if(!input[row])
+        {
+            input[row] = [];
+        }
+        input[row].push(data[i].value);
+    }
+
+    for (var i = 0; i < input.length; i++)
     {
         sum = 0;
-        for (var j = 0; j < data[i].length; j++)
+        for (var j = 0; j < input[i].length; j++)
         {
-            sum += +data[i][j].value;
+            sum = sum + parseInt(input[i][j]);
         }
-        sum = sum / data[i].length;
-        for (var j = 0; j < data[i].length; j++)
+        sum = sum / input[i].length;
+        for (var j = 0; j < input[i].length; j++)
         {
-            if (data[i][j].value > sum)
+            if (input[i][j] > sum)
             {
-                result[] = data[j].value;
+                result.push(input[i][j]);
             }
         }
     }
 
     document.getElementById("rez").value = result.join(", ");
 }
+
+function getRandomInt(min, max)
+{
+    return Math.round(min - 0.5 + Math.random() * (max - min));
+}
+
+function Random()
+{
+    document.getElementById("rez2").value = getRandomInt(document.getElementById("min").value, document.getElementById("max").value);
+}
+
+function getArray(n)
+{
+    var max = 100;
+    var min = 0;
+    let arr = new Array(n);
+    for (let i = 0; i < arr.length; i++)
+    {
+        arr[i] = getRandomInt(min, max);
+    }
+    return arr;
+}
+
+function compareNumeric(a, b)
+{
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+}
+
+function getResultArray(a)
+{
+    if (a.length > 0)
+    {
+        return a.sort(compareNumeric)
+    }
+}
+
+function mat(array)
+{
+    var n = Math.sqrt(array.length)
+    var c = 0;
+    var matrix = [];
+    for (i = 0; i < n; i++)
+    {
+        matrix[i] = [];
+    }
+
+    for (row = 0; row <= n - 1; row++)
+    {
+        if (row % 2 == 1)
+        {
+            for (i = 0; i < n; i++)
+            {
+                matrix[i][row] = array[c++];
+            }
+        }
+        else
+        {
+            for (i = n - 1; i >= 0; i--)
+            {
+                matrix[i][row] = array[c++];
+            }
+        }
+    }
+    return matrix;
+}
+
+(function ()
+{
+    var table = document.createDocumentFragment();
+    var arr = mat(getResultArray(getArray(16)));
+    for (var i = 0; i < 4; i++)
+    {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < 4; j++)
+        {
+            var td = document.createElement('td');
+            td.innerHTML = arr[i][j];
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    document.getElementById('matrix').appendChild(table);
+})();
